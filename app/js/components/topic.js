3c83ac57b05a46ebcb839e85be0e69aeaa3e617e
Ivan Matiishyn <matiyishyn.ivan@gmail.com>
2016-01-07 13:31:17 +0100

    image-store

--- app/js/components/topic.js
+++ app/js/components/topic.js
@@ -1,11 +1,37 @@
 import React from 'react';
 import { Router, Route, Link } from 'react-router';
+import Reflux from 'reflux';
+import Actions from '../actions';
+import ImageStore from '../stores/image-store';
 
-export default class Topic extends React.Component {
+let Topic = React.createClass({
+    mixins: [
+        Reflux.listenTo(ImageStore, 'onChange')
+    ],
+
+    getInitialState() {
+        return {
+            images: []
+        }
+    },
+
+    componentWillMount() {
+        console.log('will mount');
+        Actions.getImages(this.props.params.id);
+    },
+
     render() {
         return (
-            <div>Topic #{this.props.id}</div>
+            <div>
+                Topic #{this.props.params.id}
+            </div>
         );
+    },
+
+    onChange(event, images) {
+        this.setState({images: images});
     }
-}
+});
+
+export default Topic;
 
---
+++ app/js/stores/image-store.js
@@ -0,0 +1,18 @@
+import Api from '../utils/api';
+import Reflux from 'reflux';
+import Actions from '../actions';
+
+export default Reflux.createStore({
+    listenables: [Actions],
+    getImages(topicId) {
+        console.log(topicId);
+        return Api.get('topics/' + topicId)
+            .then(json => {
+                this.images = json.data;
+                this.triggerChange()
+            });
+    },
+    triggerChange() {
+        this.trigger('change', this.images);
+    }
+});
